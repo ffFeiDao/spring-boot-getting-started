9249f7ce011ae10fbfd8b6011717ecfd13c5e3d9
ffFeiDao <57787783+ffFeiDao@users.noreply.github.com>
2019-11-15 15:10:00 +0800

    Update local.sh

--- local.sh
+++ local.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 set -e
-export LC_APP_ID=
-export LC_APP_KEY=
-export LEANCLOUD_APP_ID=
-export LEANCLOUD_APP_KEY=
-export LEANCLOUD_APP_MASTER_KEY=
+export LC_APP_ID="jzlQgFhRyDNR1iRv6f3clC0l-gzGzoHsz"
+export LC_APP_KEY="M1RAH7QazcsoLHvFnTvMiqnA"
+export LEANCLOUD_APP_ID="jzlQgFhRyDNR1iRv6f3clC0l-gzGzoHsz"
+export LEANCLOUD_APP_KEY="M1RAH7QazcsoLHvFnTvMiqnA"
+export LEANCLOUD_APP_MASTER_KEY="t59U7taXwmt3ST2V8gqvHUt8"
 export LEANCLOUD_APP_PORT=3000
 export LEANCLOUD_APP_ENV=development
 java -jar ./target/spring-boot-getting-started-0.0.1-SNAPSHOT.jar
